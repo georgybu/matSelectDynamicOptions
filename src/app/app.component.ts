3df6dbf0e8799145e421fcd0da12ecfd485d95c4
Georgy Bunin <georgyb@mprest.com>
2020-02-25 14:36:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'example1';
+  public options = {};
+
+  public displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'select'];
+
+  public dataSource: any[] = [
+    {position: 1, id: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', version: 'b'},
+    {position: 2, id: 2, name: 'Helium', weight: 4.0026, symbol: 'He', version: 3},
+  ];
+
+  constructor(private http: HttpClient) {
+  }
+
+  onOpenedChange($event: boolean, element: any) {
+    console.log($event, element);
+    this.http.get(`/assets/${element.name}.json`).subscribe((response) => {
+      this.options[element.id] = response;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
